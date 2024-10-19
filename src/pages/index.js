70adf7602f358938158d076bd758ec5734ac6e64
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-18 21:18:07 -0500

    Imported Navbar and CatFacts components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
+import CatFacts from "./CatFacts";
+import NavBar from "./Navbar";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl text-center">Welcome to the Cat App!</h1>
+      <NavBar />
+      <h1 className="text-4xl text-center font-bold">
+        Welcome to the Cat App!
+      </h1>
+      <CatFacts />
     </div>
   );
 }
