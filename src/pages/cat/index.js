f7d571a174fdc3d0a2ced5570899287fc24c746d
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-18 21:22:10 -0500

    Imported Navbar and CatDetails components

--- src/pages/cat/index.js
+++ src/pages/cat/index.js
@@ -1,11 +1,23 @@
 import catData from "../../data/cat-data.json";
+import NavBar from "../Navbar";
 import CatCard from "./CatCard";
+import CatDetails from "./CatDetails";
 export default function Cat() {
   console.log(catData);
   return (
     <>
-      <h1>Hi, I'm a cat.</h1>
+      <NavBar />
+      <h1 className="text-4xl text-center font-bold">Cat of the Day!</h1>
       <CatCard name={catData.name} imgSrc={catData.image_link} />
+      <CatDetails
+        origin={catData.origin}
+        expectancy={`${catData.min_life_expectancy} - ${catData.max_life_expectancy} years`}
+        familyFriendly={catData.family_friendly}
+        shedding={catData.shedding}
+        health={catData.general_health}
+        playfulness={catData.playfulness}
+        grooming={catData.grooming}
+      />
     </>
   );
 }
