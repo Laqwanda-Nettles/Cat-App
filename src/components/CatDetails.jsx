077ca4620d6a0288af45f3ee4ccccc5f47d02fe8
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-23 22:44:38 -0500

    moved component into a component directory

---
+++ src/components/CatDetails.jsx
@@ -0,0 +1,55 @@
+import StarRating from "./StarRating";
+
+export default function CatDetails({
+  origin,
+  expectancy,
+  familyFriendly,
+  health,
+  shedding,
+  playfulness,
+  grooming,
+}) {
+  return (
+    <div className="mt-10 p-4 bg-[#70CEE6] w-full text-[#01141E]">
+      <h2 className="font-semibold text-4xl text-center drop-shadow">
+        About this cat:
+      </h2>
+      <div className="w-[290px] p-2 m-auto text-xl flex flex-col items-start">
+        <p>
+          <span className="font-semibold">Origin: </span>
+          {origin}
+        </p>
+        <p>
+          <span className="font-semibold">Life Expectancy: </span>
+          {expectancy}
+        </p>
+      </div>
+
+      <h3 className="text-3xl font-semibold text-center drop-shadow">
+        Cat Ratings:
+      </h3>
+      <div className="w-40 m-auto text-xl flex flex-col items-start">
+        <p>
+          <span className="font-semibold">Family Friendly: </span>
+          <StarRating rating={familyFriendly} />
+        </p>
+        <p>
+          <span className="font-semibold">General Health: </span>
+          <StarRating rating={health} />
+        </p>
+        <p>
+          <span className="font-semibold">Shedding: </span>
+          <StarRating rating={shedding} />
+        </p>
+        <p>
+          <span className="font-semibold">Playfulness: </span>
+          <StarRating rating={playfulness} />
+        </p>
+        <p>
+          <span className="font-semibold">Grooming: </span>
+          <StarRating rating={grooming} />
+        </p>
+      </div>
+    </div>
+  );
+}
