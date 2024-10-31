cfed4be4c42746a8ee5d56443a8bc58ec11c82d0
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-30 19:34:18 -0500

    Added API route + filled it with mock data

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({
+    movies: [
+      {
+        name: "Mulan",
+        quote:
+          "The flower that blooms in adversity is the most rare and beautiful of all.",
+      },
+      {
+        name: "The Lion King",
+        quote:
+          "The past can hurt. But the way I see it, you can either run from it or learn from it.",
+      },
+      {
+        name: "Saving Mr. Banks",
+        quote:
+          "Disappointments are to the soul what thunderstorms are to the air.",
+      },
+    ],
+  });
 }
