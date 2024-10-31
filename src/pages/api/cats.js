b933676c7ec692bfe2a73d095006a81fba13659a
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-30 22:52:00 -0500

    Implemented catquery parameter handling + extracted more cat data from api

--- src/pages/api/cats.js
+++ src/pages/api/cats.js
@@ -4,13 +4,15 @@ async function getCatData(catquery) {
   const result = await fetch(URL, {
     headers: { "X-Api-Key": API_KEY },
   });
+
   const data = await result.json();
   return data;
 }
 
 export default async function handler(req, res) {
+  const { catquery } = req.query;
   try {
-    const cat = await getCatData("british");
+    const cat = await getCatData(catquery);
 
     const responseData = {};
     if (cat.length > 0) {
@@ -18,6 +20,13 @@ export default async function handler(req, res) {
       responseData.origin = cat[0].origin;
       responseData.length = cat[0].length;
       responseData.image = cat[0].image_link;
+      responseData.min_life = cat[0].min_life_expectancy;
+      responseData.max_life = cat[0].max_life_expectancy;
+      responseData.health = cat[0].general_health;
+      responseData.family_friendly = cat[0].family_friendly;
+      responseData.shedding = cat[0].shedding;
+      responseData.grooming = cat[0].grooming;
+      responseData.playful = cat[0].playfulness;
     } else {
       responseData.error = "No Cats Were Found";
     }
--- src/pages/cat/index.js
+++ src/pages/cat/index.js
@@ -10,15 +10,18 @@ export default function Cat() {
   const [cat, setCat] = useState({});
 
   //Declaring 'loading' state to manage loading visibility
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  const [catQuery, setCatQuery] = useState(""); // State to store cat breed query
 
   // URL for the API to fetch cat data
-  const url = "https://cats-cats-cats-demo.deno.dev/cats/bombay";
+  const url = "/api/cats";
 
   // Asynchronous function to fetch data from the API
-  async function fetchCat() {
+  async function fetchCat(catquery) {
+    setLoading(true);
     try {
-      const result = await fetch(url);
+      const result = await fetch(url + `?catquery=${catquery}`);
       const data = await result.json();
       // Updating the 'cat' state with the fetched data
       setCat(data);
@@ -31,16 +34,42 @@ export default function Cat() {
     }
   }
 
-  // useEffect to trigger the fetchCat function once the component is mounted
-  useEffect(() => {
-    // Invoking fetchCat function to get data when component loads
-    fetchCat();
-  }, []); // Empty dependency array ensures this only runs once
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (catQuery) {
+      fetchCat(catQuery);
+    }
+  };
+
+  // // useEffect to trigger the fetchCat function once the component is mounted
+  // useEffect(() => {
+  //   // Invoking fetchCat function to get data when component loads
+  //   fetchCat();
+  // }, []); // Empty dependency array ensures this only runs once
 
   return (
     <>
       <NavBar />
       <h1 className="text-4xl text-center font-bold">Cat of the Day!</h1>
+
+      <form onSubmit={handleSubmit} className="flex justify-center mt-4">
+        <label className="text-lg m-2 text-[#219ebc] font-semibold">
+          Search For A Cat:
+          <input
+            type="text"
+            value={catQuery}
+            onChange={(e) => setCatQuery(e.target.value)}
+            placeholder="Enter cat breed..."
+            className="border p-2 ml-2 rounded text-[#023e8a]"
+          />
+          <button
+            type="submit"
+            className="ml-2 p-2 bg-[#0077b6] text-white rounded hover:bg-[#00b4d8] shadow-md shadow-amber-400 hover:shadow-amber-300 hover:scale-105 duration-300"
+          >
+            Search
+          </button>
+        </label>
+      </form>
 
       {loading ? (
         <div className="flex justify-center my-10">
@@ -51,14 +80,14 @@ export default function Cat() {
         </div>
       ) : (
         <>
-          <CatCard name={cat.name} imgSrc={cat.image_link} />
+          <CatCard name={cat.name} imgSrc={cat.image} />
           <CatDetails
             origin={cat.origin}
-            expectancy={`${cat.min_life_expectancy} - ${cat.max_life_expectancy} years`}
+            expectancy={`${cat.min_life} - ${cat.max_life} years`}
             familyFriendly={cat.family_friendly}
             shedding={cat.shedding}
-            health={cat.general_health}
-            playfulness={cat.playfulness}
+            health={cat.health}
+            playfulness={cat.playful}
             grooming={cat.grooming}
           />
         </>
