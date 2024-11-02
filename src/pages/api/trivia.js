482a5aca93cbc6a2ca21544a2b545d505613fddb
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-11-02 01:07:47 -0500

    added error handling to trivia api route

--- src/pages/api/trivia.js
+++ src/pages/api/trivia.js
@@ -1,17 +1,32 @@
 async function getAnimeTrivia() {
-  const response = await fetch(
-    "https://opentdb.com/api.php?amount=5&category=31"
-  );
-  const data = await response.json();
+  const url = "https://opentdb.com/api.php?amount=1&category=31";
+  const result = await fetch(url);
+
+  if (!result.ok) {
+    throw new Error(`API responded with status: ${result.status}`);
+  }
+  const data = await result.json();
   return data;
 }
 
 export default async function handler(req, res) {
-  const trivia = await getAnimeTrivia();
-  console.log(trivia);
-  res.status(200).json({
-    question: trivia.results[0].question,
-    difficulty: trivia.results[0].difficulty,
-    answer: trivia.results[0].correct_answer,
-  });
+  try {
+    const trivia = await getAnimeTrivia();
+
+    if (!trivia.results || trivia.results.length === 0) {
+      throw new Error("No trivia results found");
+    }
+
+    const responseData = {
+      question: trivia.results[0].question,
+      difficulty: trivia.results[0].difficulty,
+      answer: trivia.results[0].correct_answer,
+    };
+    res.status(200).json(responseData);
+  } catch (error) {
+    console.error("Error fetching trivia data: ", error);
+    res
+      .status(500)
+      .json({ error: error.message || "Failed to fetch trivia data." });
+  }
 }
