8a27ea887dd0300d13488cd5722f83d079d45cfc
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-11-02 00:59:05 -0500

    added error handling + image data

--- src/pages/api/pokemon.js
+++ src/pages/api/pokemon.js
@@ -1,5 +1,5 @@
-async function getPokemon() {
-  const url = "https://pokeapi.co/api/v2/pokemon/squirtle";
+async function getPokemon(name) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
   const result = await fetch(url);
   const data = await result.json();
   return data;
@@ -7,12 +7,21 @@ async function getPokemon() {
 
 // handles requests and response
 export default async function handler(req, res) {
-  const pokemondata = await getPokemon();
-  console.log(pokemondata);
-  res.status(200).json({
-    name: pokemondata.name,
-    ability: pokemondata.abilities[0].ability.name,
-    secondAbility: pokemondata.abilities[1].ability.name,
-    type: pokemondata.types[0].type.name,
-  });
+  const { name } = req.query;
+
+  try {
+    const pokemondata = await getPokemon(name);
+    const responseData = {
+      name: pokemondata.name,
+      ability: pokemondata.abilities[0].ability.name,
+      secondAbility:
+        pokemondata.abilities[1]?.ability.name || "No second ability",
+      type: pokemondata.types[0].type.name,
+      img: pokemondata.sprites.front_default,
+    };
+    res.status(200).json(responseData);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch Pokemon data." });
+    console.error("Error fetching Pokemon data: ", error);
+  }
 }
